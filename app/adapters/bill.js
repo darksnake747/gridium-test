4aebc558a2702d92ebe0272c622cae4e4f9f8b7b
darksnake747 <eric.chamberlin@live.com>
2025-10-30 05:06:52 -0400

    Demo branch

--- app/adapters/bill.js
+++
@@ -1,7 +0,0 @@
-import ApplicationAdapter from './application';
-
-export default class BillAdapter extends ApplicationAdapter {
-  pathForType() {
-    return 'services/2080448990210/bills';
-  }
-}
--- app/components/app-header.gjs
+++
@@ -1,6 +0,0 @@
-import MainMenu from './main-menu';
-
-<template>
-  <h1 class="app-title">Gridium Code Test</h1>
-  <MainMenu></MainMenu>
-</template>
