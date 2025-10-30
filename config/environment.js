dceb2f2acbc5054d3964f970746ead5a9b4d97b5
darksnake747 <eric.chamberlin@live.com>
2025-10-30 05:19:01 -0400

    Attempt to fix build on github

--- config/environment.js
+++ config/environment.js
@@ -42,6 +42,7 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV.rootURL = '/gridium-test/';
   }
 
   return ENV;
